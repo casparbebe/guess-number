7907cf6456d6251faa5e6a5bc83bd1ec85b3597a
Caspar Chang <caspar2201@gmail.com>
2021-06-02 16:37:17 +0800

    final version

--- guess.py
+++ guess.py
@@ -1,10 +1,19 @@
-import random
+# 產生一個隨機整數1~100(不要印出來)
+# 讓使用者重複輸入數字去猜
+# 猜對的話 印出 "終於猜對了!"
+# 猜錯的話 要告訴他 比答案大/小
 
-r = random.randint(1, 100)
+import random
+start = input('請決定隨機數的開始值(請打阿拉伯數字): ')
+end = input('請決定隨機數的結束值(請打阿拉伯數字): ')
+start = int(start)
+end = int(end)
+
+r = random.randint(start, end)
 count = 0
 while True:
 	count += 1 # count = count + 1
-	number = input('請猜一個1~100的數字: ')
+	number = input('請猜一個數字: ')
 	number = int(number)
 	if number == r:
 		print('終於猜對了!')
